a11d35770dbb247f9c57c0b541e669d836a15a53
albus_percival <sergeytarasevich2012@gmail.com>
2025-02-28 15:06:01 +0100

    initial commit

---
+++ nav-aid-tracker/src/main/java/com/navaidtracker/NavAidTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.navaidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavAidTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavAidTrackerApplication.class, args);
+	}
+
+}
---
+++ nav-aid-tracker/src/test/java/com/navaidtracker/NavAidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navaidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavAidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
